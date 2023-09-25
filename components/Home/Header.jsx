2f04cfe659a3c40727a3da11ae0f4af8599012c4
Pushkar Yadav <96358784+pushkarydv@users.noreply.github.com>
2023-09-25 23:35:06 +0530

    Layout Fix

---
+++ components/Home/Header.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Header() {
+  return (
+    <div className='flex flex-row justify-center items-center'>
+        <h1>Contributor Profiles</h1>
+    </div>
+  )
+}
+
+export default Header
