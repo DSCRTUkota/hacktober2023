82cec26daacb0bac215036a0ac8feb4e724facf4
Pushkar Yadav <96358784+pushkarydv@users.noreply.github.com>
2023-09-29 19:50:37 +0530

    FIx: Removed Failed API

--- app/api/profiles/route.js
+++
@@ -1,8 +0,0 @@
-import { NextResponse } from 'next/server'
-import fs from 'fs';
-export async function GET(request) {
-  const jsonData = fs.readFileSync('../../../contributors/profiles.json', 'utf8');
-  const profiles = await JSON.parse(jsonData);
- 
-  return NextResponse.json({ message: 'Hello World' })
-}
